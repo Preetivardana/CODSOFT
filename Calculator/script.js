1605aac8b4b3ead54b3dbbadec55e801cb122d73
Preetivardana <preetivardanaa@gmail.com>
2026-05-10 11:52:18 +0530

    Implement toggleSign function in calculator
    Added toggleSign function to change the sign of the displayed number.

--- Calculator/script.js
+++ Calculator/script.js
@@ -15,6 +15,18 @@ function deleteLast() {
   display.value = display.value.slice(0, -1);
 }
 
+function toggleSign() {
+
+  if (display.value.startsWith("-")) {
+
+    display.value = display.value.substring(1);
+
+  } else {
+
+    display.value = "-" + display.value;
+  }
+}
+
 function calculate() {
 
   try {
